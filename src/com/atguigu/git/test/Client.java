0e9bbc3acc2a6b4e542bdcbe8f401601c7687d34
glh1121 <glh1121@163.com>
2016-08-25 23:02:36 +0800

    p2 update
    p2update

--- src/com/atguigu/git/test/Client.java
+++ src/com/atguigu/git/test/Client.java
@@ -5,6 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		System.out.println("hello 0407");
 		System.out.println("hello 0407 02");
+		System.out.println("hello p2");
 	}
 	
 }
